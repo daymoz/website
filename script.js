a1a402b857acdaadb8bbeb3e147a5514af51ae31
Daymo's <damienletellier75018@gmail.com>
2018-10-02 01:25:02 +0200

    Bulle logic

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-var $baloon_1 = document.getElementById('item-1');
+var $item_1 = document.getElementById('item-1');
+var $baloon_1 = document.getElementById('baloon_1');
 var $area = document.getElementById('area');
 
-var item1Width = $baloon_1.clientWidth;
+var item1Width = $item_1.clientWidth;
+var item1Height = $item_1.clientHeight;
 
 var areaHeight = $area.clientHeight;
 var areaWidth = $area.clientWidth;
 
 var areaWidthAdjustMin = (areaWidth/3) - (areaWidth/4); 
-var areaWidthAdjustMax = (areaWidth/3) - (item1Width/2); 
+var areaWidthAdjustMax = (areaWidth/3) - (item1Width/3); 
 
 var areaHeightAdjustMin = areaHeight - ((areaHeight*2)/3);
 var areaHeightAdjustMax = areaHeight - (areaHeight/3);
 
-$baloon_1.style.top = getRandomIntInclusive(areaHeightAdjustMin, areaHeightAdjustMax)+"px";
-$baloon_1.style.left = getRandomIntInclusive(areaWidthAdjustMin, areaWidthAdjustMax)+"px";
-
-$baloon_1.addEventListener('mousemove', move);
+$item_1.style.top = getRandomIntInclusive(areaHeightAdjustMin, areaHeightAdjustMax)+"px";
+$item_1.style.left = getRandomIntInclusive(areaWidthAdjustMin, areaWidthAdjustMax)+"px";
 
 window.setInterval(function(){
-    $baloon_1.style.transform = "translate("+getRandomIntInclusive(10, 15)+"px,"+getRandomIntInclusive(10, 15)+"px)";
-  }, 100);
+  $item_1.style.transform = "translate("+getRandomIntInclusive(-15, 15)+"px,"+getRandomIntInclusive(-15, 15)+"px)";
+}, 2000);
+
+$baloon_1.addEventListener('mousemove', move);
+$baloon_1.addEventListener('mouseout', reset);
+$item_1.addEventListener('mouseenter', clearInterval());
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -28,14 +32,12 @@ function getRandomIntInclusive(min, max) {
 }
 
 function move(e){
-  //console.log(this);
-  // console.log(this.offsetLeft);
-  // console.log(this.offsetTop);
-  // console.log(e.clientX);
-  // console.log(e.clientY);
-  mouseX = e.clientX - this.offsetLeft;
-  mouseY = e.clientY - this.offsetTop;
-  console.log(mouseX);
-  console.log(mouseY);
-  
+  var mouseX = e.clientX - this.parentNode.offsetLeft - item1Width;
+  var mouseY = e.clientY - this.parentNode.offsetTop - item1Height/2;
+  this.style.transform = "translate(-"+Math.round(mouseX/6)+"px, -"+Math.round(mouseY/6)+"px)";
+
+}
+
+function reset() {
+  this.style.transform = "translate(0, 0)";
 }
