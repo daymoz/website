2600bafa5055f58592ec35a66177a218b06ab1fe
daymoz <damienletellier75018@gmail.com>
2018-10-17 18:50:31 +0200

    Intro

--- crypteffect.js
+++ crypteffect.js
@@ -94,7 +94,6 @@ function CryptEffect(element) {
           clearInterval(timer);
           clearInterval(secTimer);
           el.innerHTML = currentChar;
-          init();
         }
       }, 1000);
       var timer = setInterval(function() {
@@ -118,27 +117,37 @@ var head = new CryptEffect($head);
 head.letterIt();
 head.shuffle();
 
+setTimeout(function() {
+  init();
+}, 6000);
+
 var pageWrap = document.getElementById( 'pagewrap' ),
   pages = [].slice.call( pageWrap.querySelectorAll( 'div.container' ) ),
   currentPage = 0,
   triggerLoading = [].slice.call( pageWrap.querySelectorAll( 'a.pageload-link' ) ),
   preloaderAnim = document.getElementById( 'preloader-anim' );
+  preloader = document.getElementById( 'preloader' );
   loader = new SVGLoader( document.getElementById( 'loader' ), { speedIn : 400, easingIn : mina.easeinout } );
 
 function init() {
-  preloaderAnim.classList.add('show');
+  setTimeout(function() {
+    preloader.classList.add('hide');
+  }, 250);
+  setTimeout(function() {
+    preloaderAnim.classList.add('show');
+  }, 230);
   // after some time hide loader
   loader.show();
   setTimeout( function() {
     loader.hide();
     preloaderAnim.classList.remove('show')
-
+    preloader.hide();
     //classie.removeClass( pages[ currentPage ], 'show' );
-    pages[ currentPage ].classList.remove('show');
-    // update..
-    currentPage = currentPage ? 0 : 1;
-    pages[ currentPage ].classList.add('show');
+    // pages[ currentPage ].classList.remove('show');
+    // // update..
+    // currentPage = currentPage ? 0 : 1;
+    // pages[ currentPage ].classList.add('show');
     //classie.addClass( pages[ currentPage ], 'show' );
 
-  }, 2000 );	
+  }, 1000 );	
 }
