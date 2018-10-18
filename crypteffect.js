9802fe3bd249e8c4e7e4a2f1a811634831006fee
daymoz <damienletellier75018@gmail.com>
2018-10-18 18:19:33 +0200

    intro animation

--- crypteffect.js
+++ crypteffect.js
@@ -164,7 +164,7 @@ function init() {
   loader.show();
   setTimeout(function() {
     preloaderAnim.classList.remove('show');
-  }, 2850000);
+  }, 2850);
   setTimeout( function() {
     loader.hide();
     preloader.hide();
@@ -175,5 +175,5 @@ function init() {
     // pages[ currentPage ].classList.add('show');
     //classie.addClass( pages[ currentPage ], 'show' );
 
-  }, 28000000);	
+  }, 2800);	
 }
